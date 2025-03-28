04183a16419c0a75a490dc3160bda4db65abe088
kya512 <mailto:your.email@example.com>
2025-03-28 16:33:55 +0100

    modification de l'index

--- script.js
+++
@@ -1,8 +0,0 @@
-const element = document.getElementById('element');
-
-window.addEventListener('scroll', () => {
-    const rect = element.getBoundingClientRect();
-    if (rect.top < window.innerHeight && rect.bottom > 0) {
-        element.classList.add('visible');
-    }
-});
